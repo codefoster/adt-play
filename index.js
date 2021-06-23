41457442fd5c94dd5c342586e1e5bc259221d493
Jeremy Foster <jeremy.foster@live.com>
2021-06-23 15:03:17 -0700

    forgot what these changes were for

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 "use strict";
 //docs for digital-twins-core here: https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/digitaltwins/digital-twins-core
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -12,7 +19,7 @@ const path_1 = require("path");
 //configuration
 let defaultConfig = {
     modelsFolder: "./models",
-    twinsFolder: "./twins"
+    twinsFolder: "./twins",
 };
 let args = [
     { name: "digitalTwinsEndpointUrl", alias: "u", required: true },
@@ -20,10 +27,12 @@ let args = [
     { name: "twinsFolder", alias: "t" },
 ];
 let config = Object.assign(Object.assign({}, defaultConfig), command_line_args_1.default(args));
-if (!config.digitalTwinsEndpointUrl)
+if (!config.digitalTwinsEndpointUrl) {
     throw new Error("The --digitalTwinsEndpointUrl (-u) argument is required.");
+}
 let client;
-(async () => {
+main();
+async function main() {
     client = new digital_twins_core_1.DigitalTwinsClient(config.digitalTwinsEndpointUrl, new identity_1.DefaultAzureCredential());
     // DELETE MODELS
     // purge();
@@ -34,12 +43,14 @@ let client;
     // CREATE MODELS
     // createModels();
     // LIST MODELS
-    console.log((await listModels()).map(m => m.id));
+    // console.log((await listModels()).map((m) => m.id));
     // CREATE TWINS
     // createTwins();
-    // let twinJson = await readFile("./twins/widget1.json", 'utf-8');
+    // let twinJson = await readFile("./twins/widget1.json", "utf-8");
     // client.upsertDigitalTwin("widget1", twinJson);
-})();
+    // QUERY TWINS
+    console.log((await query("SELECT * FROM digitaltwins")).map(twin => twin["$dtId"]));
+}
 async function purge() {
     await deleteAllTwins();
     await sleep(10);
@@ -51,9 +62,9 @@ async function deleteAllTwins() {
     twins.forEach(async (t) => {
         //delete all relationships
         relationships = await listRelationships(t["$dtId"]);
-        await Promise.all(relationships.map(r => client.deleteRelationship(t["$dtId"], r["$relationshipId"])));
+        await Promise.all(relationships.map((r) => client.deleteRelationship(t["$dtId"], r["$relationshipId"])));
         //wait 10s
-        await new Promise(r => setTimeout(r, 10000));
+        await new Promise((r) => setTimeout(r, 10000));
         //delete twin
         await client.deleteDigitalTwin(t["$dtId"]);
     });
@@ -63,12 +74,17 @@ async function deleteAllModels() {
     models.forEach(async (m) => await client.deleteModel(m.id));
 }
 async function sleep(s) {
-    return new Promise(r => setTimeout(r, s * 1000));
+    return new Promise((r) => setTimeout(r, s * 1000));
 }
 async function createModels() {
-    let modelFiles = await promises_1.readdir(config.modelsFolder);
-    modelFiles.forEach(async (modelFile) => {
-        let model = JSON.parse(await promises_1.readFile(path_1.join(config.modelsFolder, modelFile), 'utf-8'));
+    let modelsFolderItems = await promises_1.readdir(config.modelsFolder, {
+        withFileTypes: true,
+    });
+    modelsFolderItems
+        .filter((i) => i.isFile())
+        .forEach(async (modelFile) => {
+        await promises_1.readFile(path_1.join(config.modelsFolder, modelFile.name), "utf-8");
+        let model = JSON.parse(await promises_1.readFile(path_1.join(config.modelsFolder, modelFile.name), "utf-8"));
         let modelId = model["@id"];
         await client.createModels([model]);
         console.log(`Created ${modelId}`);
@@ -77,21 +93,54 @@ async function createModels() {
 async function createTwins() {
     let twinFiles = await promises_1.readdir(config.twinsFolder);
     twinFiles.forEach(async (twinFile) => {
-        let twinJson = await promises_1.readFile(path_1.join(config.twinsFolder, twinFile), 'utf-8');
+        let twinJson = await promises_1.readFile(path_1.join(config.twinsFolder, twinFile), "utf-8");
         let twinId = JSON.parse(twinJson)["$dtId"];
         await client.upsertDigitalTwin(twinId, twinJson);
         console.log(`Created ${twinId}`);
     });
 }
 async function listModels() {
-    let models = await client.listModels();
+    var e_1, _a;
     let result = [];
-    let i = await models.next();
-    while (!i.done) {
-        result.push(i.value);
-        i = await models.next();
+    let models = await client.listModels();
+    try {
+        for (var models_1 = __asyncValues(models), models_1_1; models_1_1 = await models_1.next(), !models_1_1.done;) {
+            const model = models_1_1.value;
+            result.push(model);
+        }
+    }
+    catch (e_1_1) { e_1 = { error: e_1_1 }; }
+    finally {
+        try {
+            if (models_1_1 && !models_1_1.done && (_a = models_1.return)) await _a.call(models_1);
+        }
+        finally { if (e_1) throw e_1.error; }
     }
     return result;
+}
+async function query(query) {
+    var e_2, _a;
+    let list = [];
+    try {
+        for (var _b = __asyncValues(client.queryTwins(query)), _c; _c = await _b.next(), !_c.done;) {
+            const twin = _c.value;
+            list.push(twin);
+        }
+    }
+    catch (e_2_1) { e_2 = { error: e_2_1 }; }
+    finally {
+        try {
+            if (_c && !_c.done && (_a = _b.return)) await _a.call(_b);
+        }
+        finally { if (e_2) throw e_2.error; }
+    }
+    return list;
+    // let twins = await client.queryTwins(query);
+    // let i = await twins.next();
+    // while (!i.done) {
+    //   yield i.value;
+    //   i = await twins.next();
+    // }
 }
 async function listTwins() {
     let twins = await client.queryTwins("SELECT * FROM digitaltwins");
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 //docs for digital-twins-core here: https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/digitaltwins/digital-twins-core
 
-import { DefaultAzureCredential } from "@azure/identity";
-import { DigitalTwinsClient, DigitalTwinsModelData } from "@azure/digital-twins-core";
+import { DefaultAzureCredential, ManagedIdentityCredential } from "@azure/identity";
+import {
+  DigitalTwinsClient,
+  DigitalTwinsModelData,
+  QueryQueryTwinsResponse,
+} from "@azure/digital-twins-core";
 import commandLineArgs from "command-line-args";
 import { spawn } from "child_process";
-import { readFile, readdir } from "fs/promises";
+import { lstat, readdir, readFile } from "fs/promises";
 import { join } from "path";
 
 //configuration
 let defaultConfig = {
   modelsFolder: "./models",
-  twinsFolder: "./twins"
+  twinsFolder: "./twins",
 };
 
 let args = [
@@ -18,15 +22,17 @@ let args = [
   { name: "modelsFolder", alias: "m" },
   { name: "twinsFolder", alias: "t" },
 ];
-let config = {...defaultConfig, ...commandLineArgs(args)};
+let config = { ...defaultConfig, ...commandLineArgs(args) };
 
-if(!config.digitalTwinsEndpointUrl) throw new Error("The --digitalTwinsEndpointUrl (-u) argument is required.");
-
+if (!config.digitalTwinsEndpointUrl) {
+  throw new Error("The --digitalTwinsEndpointUrl (-u) argument is required.");
+}
 
 let client: DigitalTwinsClient;
 
-(async () => {
-  
+main();
+
+async function main() {
   client = new DigitalTwinsClient(
     config.digitalTwinsEndpointUrl,
     new DefaultAzureCredential(),
@@ -42,15 +48,18 @@ let client: DigitalTwinsClient;
 
   // CREATE MODELS
   // createModels();
-      
+
   // LIST MODELS
-  console.log((await listModels()).map(m => m.id));
-  
+  // console.log((await listModels()).map((m) => m.id));
+
   // CREATE TWINS
   // createTwins();
-  // let twinJson = await readFile("./twins/widget1.json", 'utf-8');
+  // let twinJson = await readFile("./twins/widget1.json", "utf-8");
   // client.upsertDigitalTwin("widget1", twinJson);
-})();
+
+  // QUERY TWINS
+  console.log((await query("SELECT * FROM digitaltwins")).map(twin => twin["$dtId"]));
+}
 
 async function purge() {
   await deleteAllTwins();
@@ -60,45 +69,55 @@ async function purge() {
 
 async function deleteAllTwins() {
   let twins = await listTwins();
-  let relationships:any[] = [];
-  twins.forEach(async t => {
-    
+  let relationships: any[] = [];
+  twins.forEach(async (t) => {
     //delete all relationships
     relationships = await listRelationships(t["$dtId"]);
-    await Promise.all(relationships.map(r => client.deleteRelationship(t["$dtId"], r["$relationshipId"])));
-    
+    await Promise.all(
+      relationships.map((r) =>
+        client.deleteRelationship(t["$dtId"], r["$relationshipId"])
+      ),
+    );
+
     //wait 10s
-    await new Promise(r => setTimeout(r, 10000));
-    
+    await new Promise((r) => setTimeout(r, 10000));
+
     //delete twin
     await client.deleteDigitalTwin(t["$dtId"]);
-  })
-
+  });
 }
 
 async function deleteAllModels() {
-    let models = await listModels();
-    models.forEach(async m => await client.deleteModel(m.id));
+  let models = await listModels();
+  models.forEach(async (m) => await client.deleteModel(m.id));
 }
 
 async function sleep(s) {
-  return new Promise(r => setTimeout(r, s * 1000));
+  return new Promise((r) => setTimeout(r, s * 1000));
 }
 
 async function createModels() {
-  let modelFiles = await readdir(config.modelsFolder);
-  modelFiles.forEach(async modelFile => {
-    let model = JSON.parse(await readFile(join(config.modelsFolder, modelFile), 'utf-8'));
-    let modelId = model["@id"];
-    await client.createModels([model]);
-    console.log(`Created ${modelId}`);
+  let modelsFolderItems = await readdir(config.modelsFolder, {
+    withFileTypes: true,
   });
+
+  modelsFolderItems
+    .filter((i) => i.isFile())
+    .forEach(async (modelFile) => {
+      await readFile(join(config.modelsFolder, modelFile.name), "utf-8");
+      let model = JSON.parse(
+        await readFile(join(config.modelsFolder, modelFile.name), "utf-8"),
+      );
+      let modelId = model["@id"];
+      await client.createModels([model]);
+      console.log(`Created ${modelId}`);
+    });
 }
 
 async function createTwins() {
   let twinFiles = await readdir(config.twinsFolder);
-  twinFiles.forEach(async twinFile => {
-    let twinJson = await readFile(join(config.twinsFolder, twinFile), 'utf-8');
+  twinFiles.forEach(async (twinFile) => {
+    let twinJson = await readFile(join(config.twinsFolder, twinFile), "utf-8");
     let twinId = JSON.parse(twinJson)["$dtId"];
     await client.upsertDigitalTwin(twinId, twinJson);
     console.log(`Created ${twinId}`);
@@ -106,32 +125,48 @@ async function createTwins() {
 }
 
 async function listModels(): Promise<DigitalTwinsModelData[]> {
+  let result: DigitalTwinsModelData[] = [];
+
   let models = await client.listModels();
-  let result:DigitalTwinsModelData[] = [];
-  let i = await models.next();
-  while (!i.done) {
-    result.push(i.value);
-    i = await models.next();
+
+  for await (const model of models) {
+    result.push(model);
   }
+
   return result;
+}
+
+async function query(query: string) {
+  let list: QueryQueryTwinsResponse[] = [];
+  for await (const twin of client.queryTwins(query)) {
+    list.push(twin);
+  }
+  return list;
+
+  // let twins = await client.queryTwins(query);
+
+  // let i = await twins.next();
+  // while (!i.done) {
+  //   yield i.value;
+  //   i = await twins.next();
+  // }
 }
 
 async function listTwins() {
   let twins = await client.queryTwins("SELECT * FROM digitaltwins");
 
-  let result:any[] = [];
+  let result: any[] = [];
   let i = await twins.next();
   while (!i.done) {
     result.push(i.value);
     i = await twins.next();
   }
   return result;
-
 }
 
 async function listRelationships(digitalTwinId: string) {
   let relationships = await client.listRelationships(digitalTwinId);
-  let result:any[] = [];
+  let result: any[] = [];
   let i = await relationships.next();
   while (!i.done) {
     result.push(i.value);
